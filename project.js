6a92fbaeece4a046f6b332eda5daf52fac7fe373
NurlChl <nurul.cholil2373@gmail.com>
2023-10-30 00:42:14 +0700

    menambah semua halaman dan sebagian besar responsive

---
+++ project.js
@@ -0,0 +1,42 @@
+
+objProject = [
+    {
+        namaProject : "Food Shop",
+        roleProject : "marketplace",
+        keterangan : "mobile app yang mengkonsep tentang dunia marketing dengan bertemakan makanan",
+    },
+    {
+        namaProject : "Coffee Shop",
+        roleProject : "marketplace",
+        keterangan : "sebuah ui design untuk penerapan penjualan berbagai jenis kopi. Sangat cocok untuk menemani kita minum di pagi hari",
+    },
+    {
+        namaProject : "Miod Games",
+        roleProject : "Games",
+        keterangan : "Miod Games merupakan penyedia game dalam sebuah website. Banyak sekali game yang menarik untuk dimainkan untuk kalian para gamers",
+    },
+    {
+        namaProject : "Virtual Shop",
+        roleProject : "marketplace",
+        keterangan : "Virtual Shop menyediakan berbagai macam hal mulai dari fashion, elektronik dan lain sebagainya. Website ini datang untuk memenuhi kebutuhan banyak orang.",
+    },
+]
+
+    
+    document.getElementById("namaProject1").innerHTML = objProject[0].namaProject
+    document.getElementById("namaProject2").innerHTML = objProject[1].namaProject
+    document.getElementById("namaProject3").innerHTML = objProject[2].namaProject
+    document.getElementById("namaProject4").innerHTML = objProject[3].namaProject
+
+    document.getElementById("roleProject1").innerHTML = objProject[0].roleProject
+    document.getElementById("roleProject2").innerHTML = objProject[1].roleProject
+    document.getElementById("roleProject3").innerHTML = objProject[2].roleProject
+    document.getElementById("roleProject4").innerHTML = objProject[3].roleProject
+
+    document.getElementById("keterangan1").innerHTML = objProject[0].keterangan
+    document.getElementById("keterangan2").innerHTML = objProject[1].keterangan
+    document.getElementById("keterangan3").innerHTML = objProject[2].keterangan
+    document.getElementById("keterangan4").innerHTML = objProject[3].keterangan
+
+
+
